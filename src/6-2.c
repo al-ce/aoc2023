6b185090d77fc58d5d84e169abf25dbbfd634944
al-ce <aceserani@gmail.com>
2024-01-05 00:28:19 -0600

    refactor(6-2): lines

--- src/6-2.c
+++ src/6-2.c
@@ -15,7 +15,6 @@ long fix_kerning(FILE *input_file, char *str, char *line, char *delim) {
 }
 
 void quadratic_formula(long double arr[], long a, long b, long c) {
-
     long double radicand = (b * b) - (4 * a * c);
     arr[0] = (-b + sqrt(radicand)) / 2;
     arr[1] = (-b - sqrt(radicand)) / 2;
@@ -26,7 +25,6 @@ int get_range(long double high, long double low) {
 }
 
 int main(void) {
-    printf("\nAoC 2023 - Day 6-2\n");
 
     FILE *input_file = fopen("./input/6", "r");
     if (!input_file) {
@@ -37,18 +35,17 @@ int main(void) {
     char line[256];
     char *time_str = calloc(STRMAX, sizeof(char));
     char *record_str = calloc(STRMAX, sizeof(char));
-    long time;
-    long record;
 
     fgets(line, sizeof(line), input_file);
-    time = fix_kerning(input_file, time_str, line, "Time: ");
+    long time = fix_kerning(input_file, time_str, line, "Time: ");
     fgets(line, sizeof(line), input_file);
-    record = fix_kerning(input_file, record_str, line, "Distance: ");
+    long record = fix_kerning(input_file, record_str, line, "Distance: ");
 
     long double factors[2];
     quadratic_formula(factors, -1, time, -record + 1);
 
     int ways = get_range(factors[0], factors[1]);
+    printf("\nAoC 2023 - Day 6-2\n");
     printf("\n%d ways to win\n", ways);
 
     free(time_str);
