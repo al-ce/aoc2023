3700df37bce448cf6234d924cc29dfbc7e99a70d
al-ce <aceserani@gmail.com>
2024-01-06 13:18:07 -0600

    refactor(7-1): add quicksort option

--- src/7-1.c
+++ src/7-1.c
@@ -2,80 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAXCHARS 12
 
 const char high_cards[] = {'T', 'J', 'Q', 'K', 'A'};
+
+enum hand_type {
+    ONE_PAIR = 1,
+    TWO_PAIR = 2,
+    THREE_OF_A_KIND = 3,
+    FULL_HOUSE = 4,
+    QUADS = 5,
+    FIVE_OF_A_KIND = 6
+};
 
 typedef struct {
     long score;
     long bid;
 } value;
 
-long get_file_len(FILE *file) {
-    long len = 0;
-    char line[MAXCHARS];
-    while (fgets(line, sizeof(line), file)) {
-        ++len;
-    }
-    rewind(file);
-    return len;
-}
-
-long get_card_val(char card) {
-    if (isdigit(card)) {
-        return card - '0';
-    }
-    for (int i = 0; i < 5; ++i) {
-        if (high_cards[i] == card) {
-            return i + 10;
-        }
-    }
-    return -1;
-}
-
-long update_score(long score, int idx, int card_val) {
-    int factor = 1;
-    for (int j = 0; j < 5 - idx; ++j) {
-        factor *= 13;
-    }
-    return score + card_val * factor;
-}
-
-int update_hand_type(int count, int type) {
-    return count == 5                ? 7 // five of a kind
-           : count == 4              ? 6 // quads
-           : count == 3 && type == 3 ? 5 // full house (*)
-           : count == 2 && type == 4 ? 5 // full house
-           : count == 3              ? 4 // three of a kind
-           : count == 2 && type == 2 ? 3 // two pair
-           : count == 2              ? 2 // one pair
-                                     : type;          // high card
-}
-
-void sort_hands_by_value(value *hand_values, long hands_played) {
-    for (int i = 0; i < hands_played; ++i) {
-        for (int j = i; j >= 1; --j) {
-            value h1 = hand_values[j];
-            value h2 = hand_values[j - 1];
-            long s1 = h1.score;
-            long s2 = h2.score;
-            if (s1 < s2) {
-                value temp = h1;
-                hand_values[j] = h2;
-                hand_values[j - 1] = temp;
-            }
-        }
-    }
-}
-
-long get_total_winnings(value *hand_values, long hands_played) {
-    long tot = 0;
-    for (int i = 0; i < hands_played; ++i) {
-        tot += (i + 1) * hand_values[i].bid;
-    }
-    return tot;
-}
+long get_file_len(FILE *file);
+long get_card_val(char card);
+long update_score(long score, int idx, int card_val);
+int update_hand_type(int count, int type);
+void sort_hands_by_score(value *hand_values, long hands_played);
+long get_total_winnings(value *hand_values, long hands_played);
 
 int main(void) {
     FILE *input_file = fopen("./input/7", "r");
@@ -112,11 +64,108 @@ int main(void) {
         ++counter;
     }
 
-    sort_hands_by_value(scores, hands_played);
+    sort_hands_by_score(scores, hands_played);
 
     long tot = get_total_winnings(scores, hands_played);
     printf("\nAoC 2023 - Day 7 Part 1\n");
     printf("\nTotal winnings: %lu\n", tot);
 
     fclose(input_file);
+}
+
+long get_file_len(FILE *file) {
+    long len = 0;
+    char line[MAXCHARS];
+    while (fgets(line, sizeof(line), file)) {
+        ++len;
+    }
+    rewind(file);
+    return len;
+}
+
+long get_card_val(char card) {
+    if (isdigit(card)) {
+        return card - '0';
+    }
+    for (int i = 0; i < 5; ++i) {
+        if (high_cards[i] == card) {
+            return i + 10;
+        }
+    }
+    return -1;
+}
+
+long update_score(long score, int idx, int card_val) {
+    int factor = 1;
+    for (int j = 0; j < 5 - idx; ++j) {
+        factor *= 13;
+    }
+    return score + card_val * factor;
+}
+
+int update_hand_type(int count, int type) {
+    return count == 5                ? FIVE_OF_A_KIND
+           : count == 4              ? QUADS
+           : count == 3 && type == 2 ? FULL_HOUSE
+           : count == 2 && type == 3 ? FULL_HOUSE
+           : count == 3              ? THREE_OF_A_KIND
+           : count == 2 && type == 1 ? TWO_PAIR
+           : count == 2              ? ONE_PAIR
+                                     : type;
+}
+
+value randomize_pivot(value nums[], int s, int e) {
+    int p_index = s + rand() % (e - s + 1);
+    value pivot = nums[p_index];
+    value temp = nums[e];
+    nums[e] = pivot;
+    nums[p_index] = temp;
+    return pivot;
+}
+
+void quick_sort(value nums[], int s, int e) {
+    if (s >= e) {
+        return;
+    }
+    value pivot = randomize_pivot(nums, s, e);
+
+    int left = s;
+    value tmp;
+    for (int i = s; i < e + 1; i++) {
+        if (nums[i].score < pivot.score) {
+            tmp = nums[left];
+            nums[left] = nums[i];
+            nums[i] = tmp;
+            left++;
+        }
+    }
+    nums[e] = nums[left];
+    nums[left] = pivot;
+    quick_sort(nums, s, left - 1);
+    quick_sort(nums, left + 1, e);
+}
+
+void insertion_sort(value *arr, long arr_len) {
+    for (int i = 1; i < arr_len; ++i) {
+        value curr = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j].score > curr.score) {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = curr;
+    }
+}
+
+void sort_hands_by_score(value *arr, long arr_len) {
+        /* quick_sort(arr, 0, arr_len - 1); */
+        insertion_sort(arr, arr_len);
+}
+
+long get_total_winnings(value *hand_values, long hands_played) {
+    long tot = 0;
+    for (int i = 0; i < hands_played; ++i) {
+        tot += (i + 1) * hand_values[i].bid;
+    }
+    return tot;
 }
