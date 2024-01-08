6a7ce3cde48946c08f7976910ef01e7199bbc9bf
al-ce <aceserani@gmail.com>
2024-01-07 20:41:12 -0600

    refactor(day8)

--- src/8-1.c
+++ src/8-1.c
@@ -8,6 +8,10 @@
 #define KV_SIZE 17
 
 int main(void) {
+    char instructions[MAX_SIZE];
+    char kv_line[KV_SIZE];
+    char key[4] = {0};
+    char value[9] = {0};
 
     FILE *fp = fopen("./input/8", "r");
     if (fp == NULL) {
@@ -15,14 +19,10 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-    char instructions[MAX_SIZE];
     fgets(instructions, MAX_SIZE, fp);
     fseek(fp, 1, SEEK_CUR); // consume newline
 
     HashMap *hm = hm__init__(7, 2);
-    char kv_line[KV_SIZE];
-    char key[4] = {0};
-    char value[9] = {0};
     while (fgets(kv_line, KV_SIZE, fp)) {
         strncpy(key, kv_line, 3);
         strncpy(value, kv_line + 7, 8);
@@ -31,22 +31,17 @@ int main(void) {
 
     int i = 0;
     int steps = 0;
-    int instruct_size = strlen(instructions);
-    char curr_key[4] = {0};
-    strncpy(curr_key, "AAA", 4);
-    while (strcmp(curr_key, "ZZZ")) {
-        char instruction = instructions[i];
-        char *curr_value = hm_get(hm, curr_key);
-        if (instruction == 'L') {
-            strncpy(curr_key, curr_value, 3);
-        } else if (instruction == 'R') {
-            strncpy(curr_key, curr_value + 5, 3);
-        }
+    int num_instructs = strlen(instructions);
+
+    strncpy(key, "AAA", 4);
+    while (strcmp(key, "ZZZ")) {
+        char direction = instructions[i];
+        char *curr_value = hm_get(hm, key);
+        int start_offset = direction == 'L' ? 0 : 5;
+        strncpy(key, curr_value + start_offset, 3);
         ++steps;
         ++i;
-        if (i == instruct_size - 1) {
-            i = 0;
-        }
+        i = i == num_instructs - 1 ? 0 : i;
     }
 
     printf("\nAoC 2023 Day 8 Part 1\n");
--- src/8-2.c
+++ src/8-2.c
@@ -17,75 +17,69 @@ long euclidean_gcd(long a, long b) {
 
 long get_lcm_of_array(long *arr, int arr_size) {
     for (int i = 1; i < arr_size; ++i) {
-        long a = arr[i-1];
+        long a = arr[i - 1];
         long b = arr[i];
         arr[i] = a / euclidean_gcd(a, b) * b;
     }
-    return arr[arr_size-1];
+    return arr[arr_size - 1];
+}
+
+long calculate_steps(HashMap *hm, char *instructions, char *key) {
+    int i = 0;
+    long steps = 0;
+    int num_instructs = strlen(instructions);
+
+    while (key[2] != 'Z') {
+        char *curr_value = hm_get(hm, key);
+        int start_offset = instructions[i] == 'L' ? 0 : 5;
+        strncpy(key, curr_value + start_offset, 3);
+        ++steps;
+        ++i;
+        i = i == num_instructs - 1 ? 0 : i;
+    }
+    return steps;
 }
 
 int main(void) {
+    char instructions[MAX_SIZE];
+    char kv_line[KV_SIZE];
+    char key[4] = {0};
+    char value[9] = {0};
 
     FILE *fp = fopen("./input/8", "r");
-    /* FILE *fp = fopen("./input/8-ex2", "r"); */
     if (fp == NULL) {
         perror("Attempt to open file failed\n");
         return EXIT_FAILURE;
     }
 
-    char instructions[MAX_SIZE];
     fgets(instructions, MAX_SIZE, fp);
     fseek(fp, 1, SEEK_CUR); // consume newline
 
     DynamicArray *da = da__init__(0);
     HashMap *hm = hm__init__(7, 2);
-    char kv_line[KV_SIZE];
-    char key[4] = {0};
-    char value[9] = {0};
     while (fgets(kv_line, KV_SIZE, fp)) {
         strncpy(key, kv_line, 3);
         strncpy(value, kv_line + 7, 8);
         if (key[2] == 'A') {
-            node *n = make_node(key, value);
-            da_append(da, n);
-            node_dealloc(n);
+            node n = {key, value};
+            da_append(da, &n);
         }
         hm_put(hm, key, value);
     }
 
     int size = da->size;
-    long *steps_arr = calloc(sizeof(long), size);
-
+    long steps_arr[size];
     for (int k = 0; k < size; ++k) {
-        int i = 0;
-        long steps = 0;
-        int instruct_size = strlen(instructions);
         node *n = da_get(da, k);
-        char curr_key[4] = {0};
-        char *curr_value;
-        strncpy(curr_key, n->key, 4);
-        while (curr_key[2] != 'Z') {
-            char instruction = instructions[i];
-            curr_value = hm_get(hm, curr_key);
-            if (instruction == 'R') {
-                strncpy(curr_key, curr_value + 5, 3);
-            } else if (instruction == 'L') {
-                strncpy(curr_key, curr_value, 3);
-            }
-            ++steps;
-            ++i;
-            if (i == instruct_size - 1) {
-                i = 0;
-            }
-        }
-        steps_arr[k] = steps;
+        strncpy(key, n->key, 4);
+        steps_arr[k] = calculate_steps(hm, instructions, key);
     }
 
     long lcm = get_lcm_of_array(steps_arr, size);
+
     printf("\nAoC 2023 Day 8 Part 2:\n");
     printf("LCM: %ld\n", lcm);
 
-    free(steps_arr);
     da_dealloc(da);
     hm_dealloc(hm);
     fclose(fp);
